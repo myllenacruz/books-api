e197761f8529a34d5c7293745b1da625089dd059
Myllena Cruz <myllena.ocruz@gmail.com>
2022-12-28 12:11:58 -0300

    add cors and morgan and disable x-powered-by

--- src/shared/server/app.ts
+++ src/shared/server/app.ts
@@ -1,9 +1,22 @@
 import "dotenv/config";
 import express from "express";
 import { Server as HttpServer } from "http";
+import morgan from "morgan";
+import cors from "cors";
 
 const app = express();
 const server = new HttpServer(app);
+
+/**
+ * Disable HTTP header x-powered-by for security reasons
+ */
+app.disable("x-powered-by");
+
+/**
+ * HTTP Request Middlewares
+ */
+app.use(cors({ origin: "*", optionsSuccessStatus: 200 }));
+app.use(morgan("dev"));
 
 /**
  * JSON Middleware
--- src/shared/server/index.ts
+++ src/shared/server/index.ts
@@ -3,5 +3,5 @@ import { server } from "@shared/server/app";
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
-    console.info(`Server listening on port ${PORT}`);
-})
+	console.info(`Server listening on port ${PORT}`);
+});

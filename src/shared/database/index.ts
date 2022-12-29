d154404515ded4dfa758c1b39bb885c1d839c161
Myllena Cruz <myllena.ocruz@gmail.com>
2022-12-28 22:06:47 -0300

    create connection database file

---
+++ src/shared/database/index.ts
@@ -0,0 +1,12 @@
+import { createConnection } from "typeorm";
+
+const connect = async (): Promise<void> => {
+	try {
+		await createConnection();
+	} catch (error) {
+		console.error("Error when trying to create a connection: \n", error);
+		process.exit(1);
+	}
+};
+
+connect();

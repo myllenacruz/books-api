85df533661410f07d0a7d0abb3ec0068fb729223
Myllena Cruz <myllena.ocruz@gmail.com>
2022-12-29 21:10:27 -0300

    add container register for author repositories

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -3,4 +3,9 @@ import { container } from "tsyringe";
 import { IBookRepository } from "@modules/book/repositories/IBookRepository";
 import { TypeORMBookRepository } from "@modules/book/repositories/implementations/TypeORMBookRepository";
 
+import { IAuthorRepository } from "@modules/book/repositories/IAuthorRepository";
+import { TypeORMAuthorRepository } from "@modules/book/repositories/implementations/TypeORMAuthorRepository";
+
 container.register<IBookRepository>("BookRepository", TypeORMBookRepository);
+
+container.register<IAuthorRepository>("AuthorRepository", TypeORMAuthorRepository);

27f56120fab2ce6e91136c8ff86ac1b7250eddf2
Myllena Cruz <myllena.ocruz@gmail.com>
2022-12-29 21:17:52 -0300

    updateBookService refactoring: ensure author id is provided when updateBookService is called

--- src/modules/book/services/UpdateBookService.ts
+++ src/modules/book/services/UpdateBookService.ts
@@ -2,12 +2,13 @@ import { IBookRepository } from "@modules/book/repositories/IBookRepository";
 import { Book } from "@modules/book/entities/Book";
 import { AppError } from "@shared/errors/AppError";
 import { inject, injectable } from "tsyringe";
+import { IAuthorRepository } from "@modules/book/repositories/IAuthorRepository";
 
 interface IRequest {
 	id: number;
 	name: string;
 	description: string;
-	author: string;
+	author_id: number;
 	stock_quantity: number;
 }
 
@@ -15,7 +16,9 @@ interface IRequest {
 export class UpdateBookService {
 	constructor(
 		@inject("BookRepository")
-		private bookRepository: IBookRepository
+		private bookRepository: IBookRepository,
+
+		private authorRepository: IAuthorRepository
 	) {}
 
 	public async execute(
@@ -23,21 +26,24 @@ export class UpdateBookService {
 			id,
 			name,
 			description,
-			author,
+			author_id,
 			stock_quantity
 		}: IRequest
 	): Promise<Book> {
 		const book = await this.bookRepository.findById(id);
 		const bookName = await this.bookRepository.findByName(name);
+		const author = await this.authorRepository.findById(author_id);
 
 		if (!book) throw new AppError("Book not found", 404);
 
 		if (bookName && bookName.id !== id)
 			throw new AppError("Book name already existis!", 409);
 
+		if (!author) throw new AppError("Author not found", 404);
+
 		book.name = name;
 		book.description = description;
-		book.author = author;
+		book.author_id = author_id;
 		book.stock_quantity = stock_quantity;
 
 		const updatedBook = await this.bookRepository.save(book);
